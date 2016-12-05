e7eb2ea322134f670998278698ce23a66070cb5a
James Lloyd <james.lloyd@woolpert.com>
2016-12-05 15:01:12 -0500

    Fresh Add

---
+++ JamesRiverHBC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JamesRiverHBC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Club History.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "We're heating the mash water now.";
+
+            return View();
+        }
+
+        public ActionResult Meetings()
+        {
+            ViewBag.Message = "Monthly Club Meetings";
+
+            return View();
+        }
+    }
+}
